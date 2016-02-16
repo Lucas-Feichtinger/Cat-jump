8cc2463f6b5b830fe921c58641b4e356c45a1892
Lucas Feichtinger <fei16940@spengergasse.at>
2016-02-16 10:41:21 +0100

    TODOs hinzugefügt

--- src/catjump/Main.java
+++ src/catjump/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hallo");
 
+		// TODO next Florian week: Map initialisieren. Dia.
+		// TODO next Lucas week: Baum und Gras zeichnen. Map initialisieren.
 	}
 
 }
